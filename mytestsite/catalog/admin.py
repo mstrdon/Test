b5e6c75bd806a4497edf82b190ce71ab9487412e
mstrdon <75879286+mstrdon@users.noreply.github.com>
2020-12-12 14:52:49 +0600

    Homework
    Homework

---
+++ mytestsite/catalog/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+
+#admin.site.register(Author)
+#admin.site.register(BookInstance)
+#admin.site.register(Genre)
+#admin.site.register(Language)
+
+admin.site.register(Genre)
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+class BooksInstanceInline(admin.TabularInline):
+    list_filter = ('status', 'due_back')
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+    
+admin.site.unregister(Author)
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ['status', 'due_back']
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
